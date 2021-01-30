15e5d6fae75310ab14dd70dab05f020c17f3b013
Prof. (Dr) Hiren Patel <hbpatel1976@gmail.com>
2021-01-30 13:57:37 +0530

    Add files via upload

---
+++ displayPattern.java
@@ -0,0 +1,18 @@
+/* Display.java */
+class displayPattern
+    	 {
+       public static void main(String args[])
+                {
+                System.out.println("Screen Display");
+                for(int i=1; i<9; ++i)
+                        {
+                        for(int j=1; j<=i; ++j)
+                                {
+                                System.out.print(" ");
+                                System.out.print(i);
+                                }
+                        System.out.print("\n");
+                        }
+                System.out.println("Screen Display Done");
+                }
+			}
