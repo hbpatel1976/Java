6ed45f2a3513820a4afa0cf92ee87e75ab6049d9
Prof. (Dr) Hiren Patel <hbpatel1976@gmail.com>
2021-01-30 14:03:06 +0530

    Add files via upload

---
+++ Relation.java
@@ -0,0 +1,18 @@
+/* Relation.java */
+class Relation
+        {
+        public static void main(String args[])
+                {
+                float a=15.0F,b=20.75F,c=15.05F;
+                System.out.println("a = "+a);
+                System.out.println("b = "+b);
+                System.out.println("c = "+c);
+                System.out.println("a < b = "+(a<b));
+                System.out.println("a > b = "+(a>b));
+                System.out.println("a == c = "+(a==b));
+                System.out.println("a <= c = "+(a<=c));
+                System.out.println("a >= b = "+(a>=b));
+                System.out.println("b != c = "+(b!=c));
+                System.out.println("b == a+c = "+(b==a+c));
+                }
+        }
